32f3890a44241b1a8eafd3e288c2170b138ddb18
zack <zcastaneda@gmail.com>
2023-12-17 11:17:35 -0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 #include <Arduino.h>
-#include "ky028.h"
 
-int DIG_PIN = 0;
-int ANALOG_PIN = A0;
-double t;
-// char TEMP_PIN = D0;
-// const int TEMP_PIN = 5;
-KY028 temp_1(DIG_PIN,ANALOG_PIN);
 
+#define LED 2
+const int analogInPin = A0;
+const int sensor_value = 0;
 void setup() {
   // put your setup code here, to run once:
 
-  // pinMode(DIG_PIN, INPUT);
-  // digitalWrite(TEMP_PIN, HIGH);
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(analogInPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  t = temp_1.get_temp();
-  // Serial.println(digitalRead(DIG_PIN));
-  // Serial.println(analogRead(ANALOG_PIN));
-  delay(100);
+  // Serial.println(digitalRead(HB_100));
+  Serial.println("test_value");
+  Serial.println(analogRead(analogInPin));
+  Serial.println("   ");
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
 }
